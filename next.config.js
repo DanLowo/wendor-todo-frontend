80121cc1fd15766b4c0fd19a06e8b92771549420
danlowo <danielolulowo3@gmail.com>
2023-12-29 19:48:23 +0100

    additional fix

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  experimental: {
+    incrementalCacheHandlerPath: process.env.CUSTOM_CACHE_HANDLER
+      ? require.resolve('./cache-handler.js')
+      : undefined,
+  }
 }
 
 module.exports = nextConfig
